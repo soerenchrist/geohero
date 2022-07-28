b86cba327896ab727239d333233ce97cff62f7d0
Sören Christ <soeren.christ@bcmsolutions.de>
2022-07-28 13:46:47 +0200

    Measure seconds

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps, NextPage } from "next";
 import dynamic from "next/dynamic";
-import { useEffect, useMemo, useState } from "react";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Container from "../../components/common/container";
 import Meta from "../../components/common/meta";
 import CorrectCountriesDisplay from "../../components/game/correctCountriesDisplay";
@@ -18,6 +19,33 @@ import { trpc } from "../../utils/trpc";
 const Map = dynamic(() => import("../../components/maps/gameMap"), {
   ssr: false,
 });
+
+const useTimer = () => {
+  const [elapsedSeconds, setElapsedSeconds] = useState(0);
+  const startTime = useRef<Date>();
+  const timer = useRef<NodeJS.Timer>();
+
+  const stop = useCallback(() => {
+    if (timer.current) {
+      clearInterval(timer.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    startTime.current = new Date();
+    timer.current = setInterval(
+      () =>
+        setElapsedSeconds(Math.floor((new Date().getTime() - startTime.current!.getTime()) / 1000)),
+      500
+    );
+
+    return () => {
+      clearInterval(timer.current);
+    };
+  }, []);
+
+  return { elapsedSeconds, stop };
+};
 
 const useCountryShape = (iso?: string) => {
   const [shapeData, setShapeData] = useState<GeoJson>();
@@ -57,6 +85,9 @@ const useCountryData = (countryIndex?: number) => {
     {
       enabled: countryIndex != null,
       refetchOnWindowFocus: false,
+      onSuccess(data) {
+        console.log(data);
+      }
     }
   );
 };
@@ -67,6 +98,7 @@ const GamePage: NextPage<{
   showDirection: boolean;
   showBorders: boolean;
 }> = ({ countryIndices, rounds, showDirection, showBorders }) => {
+  const [gameWon, setGameWon] = useState(false);
   const [correctGuesses, setCorrectGuesses] = useState<Country[]>([]);
   const [showCorrectMessage, setShowCorrectMessage] = useState(false);
   const [currentGuess, setCurrentGuess] = useState<Country>();
@@ -80,8 +112,8 @@ const GamePage: NextPage<{
 
   const { shapeData } = useCountryShape(currentGuess?.iso);
   const { data: searchedCountry } = useCountryData(currentCountryIndex);
-
-  console.log(searchedCountry);
+  const { elapsedSeconds, stop } = useTimer();
+  console.log(elapsedSeconds);
 
   useEffect(() => {
     if (
@@ -112,42 +144,69 @@ const GamePage: NextPage<{
       setTimeout(() => setShowCorrectMessage(false), 1000);
     }
   }, [currentGuess, searchedCountry, correctGuesses, currentRound]);
-
+  const router = useRouter();
   useEffect(() => {
     if (currentRound >= rounds) {
-      console.log("Finished");
       setDirection(undefined);
       setDistance(undefined);
+      setGameWon(true);
+      stop();
     }
-  }, [currentRound, rounds]);
+  }, [currentRound, rounds, router]);
 
   return (
     <>
       <Meta></Meta>
       <Container>
-        <div className="flex flex-col w-screen min-h-screen items-center gap-8">
-          <div className="lg:h-96 h-72 w-screen abolute">
-            <Map
-              center={currentGuess ?? { latitude: 49, longitude: 10 }}
-              geojson={shapeData}
-              distance={distance}
-              direction={direction}
-              showBorders={showBorders}
-            />
-            <div className="absolute top-0 right-0 px-1 py-3 text-lg font-bold">
-              <span className="bg-white p-2 rounded-lg">{currentRound + 1}/{rounds}</span>
-            </div>
-            <div className="absolute top-0 left-0 w-screen lg:h-96 h-72 pointer-events-none flex justify-center items-center flex-col text-center">
-              {showCorrectMessage && (
-                <h1 className="text-5xl animate-ping duration-1000 font-extrabold text-purple-700">
-                  Correct!
-                </h1>
-              )}
-            </div>
+        {gameWon && (
+          <div className="flex flex-col w-screen h-screen justify-center items-center">
+            <h1 className="text-3xl lg:text-8xl animate-pulse duration-1000 font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-sky-200 to-sky-500">
+              You did it!
+            </h1>
+            <h4 className="text-xl font-bold text-sky-300">
+              Congrats, it took you {elapsedSeconds} seconds!
+            </h4>
+            <button
+              className="w-48 h-16 text-lg font-medium rounded-full hover:scale-105 bg-white mt-20"
+              onClick={() => router.reload()}
+            >
+              Play again
+            </button>
           </div>
-          <CountrySearchField onCountryInput={setCurrentGuess} />
-          <CorrectCountriesDisplay countries={correctGuesses} rounds={rounds} />
-        </div>
+        )}
+        {!gameWon && (
+          <div className="flex flex-col w-screen min-h-screen items-center gap-8">
+            <div className="lg:h-96 h-72 w-screen abolute">
+              <Map
+                center={currentGuess ?? { latitude: 49, longitude: 10 }}
+                geojson={shapeData}
+                distance={distance}
+                direction={direction}
+                showBorders={showBorders}
+              />
+              <div className="absolute top-0 text-right right-0 p-2 text-lg font-bold bg-white m-2 rounded-lg">
+                <div>
+                  Round {currentRound + 1}/{rounds}
+                </div>
+                <div>
+                  {elapsedSeconds} s
+                </div>
+              </div>
+              <div className="absolute top-0 left-0 w-screen lg:h-96 h-72 pointer-events-none flex justify-center items-center flex-col text-center">
+                {showCorrectMessage && (
+                  <h1 className="text-5xl animate-ping duration-1000 font-extrabold text-purple-700">
+                    Correct!
+                  </h1>
+                )}
+              </div>
+            </div>
+            <CountrySearchField onCountryInput={setCurrentGuess} />
+            <CorrectCountriesDisplay
+              countries={correctGuesses}
+              rounds={rounds}
+            />
+          </div>
+        )}
       </Container>
     </>
   );
