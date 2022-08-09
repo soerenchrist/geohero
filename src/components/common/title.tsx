ef413bbb7d850a369046e55be5b0574f24fcf145
Sören Christ <soeren.christ@bcmsolutions.de>
2022-08-09 08:45:02 +0200

    Use title to link back to home screen

--- src/components/common/title.tsx
+++ src/components/common/title.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 const Title = () => {
   return (
-    <h1 className="text-5xl lg:text-8xl font-extrabold text-white">
-      Geo
-      <span className="bg-clip-text text-transparent bg-gradient-to-r from-accent1 to-accent2">
-        Hero
-      </span>
-    </h1>
+    <Link href="/">
+      <h1 className="text-5xl lg:text-8xl cursor-pointer font-extrabold text-white">
+        Geo
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-accent1 to-accent2">
+          Hero
+        </span>
+      </h1>
+    </Link>
   );
 };
 
